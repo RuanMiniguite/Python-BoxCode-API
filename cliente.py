b769d6931bd34f7770924d07051430822de42e13
RuanMiniguite <ruanminiguite@gmail.com>
2022-02-22 22:24:19 -0300

    :package: Menu cadastro pessoa fisica

--- cliente.py
+++ cliente.py
@@ -6,19 +6,26 @@ import os
 url = "http://127.0.0.1:8080"
 
 def menu():
-    print("-------------------:-------------------")
-    print("| 1 |  Cadastrar Usuario              |")
-    print("| 2 |  Exibir    Usuario              |")
-    print("| 3 |  Alterar   Usuario              |")
-    print("| 4 |  Excluir   Usuario              |")
-    print("-------------------:-------------------")
-    print("| 5 |  Cadastrar Projeto              |")
-    print("| 6 |  Exibir    Projeto              |")
-    print("| 7 |  Alterar   Projeto              |")
-    print("| 8 |  Excluir   Projeto              |")
-    print("-------------------:-------------------")
-    print("| 9 |  SAIR                           |")
-    print("-------------------:-------------------")
+  print("-------------------:-------------------")
+  print("| 1 |  Cadastrar Usuario              |")
+  print("| 2 |  Exibir    Usuario              |")
+  print("| 3 |  Alterar   Usuario              |")
+  print("| 4 |  Excluir   Usuario              |")
+  print("-------------------:-------------------")
+  print("| 5 |  Cadastrar Projeto              |")
+  print("| 6 |  Exibir    Projeto              |")
+  print("| 7 |  Alterar   Projeto              |")
+  print("| 8 |  Excluir   Projeto              |")
+  print("-------------------:-------------------")
+  print("| 9 |  SAIR                           |")
+  print("-------------------:-------------------")
+
+def menu1():
+  print("-------------------:-------------------")
+  print("| 1 |  Pessoa Física                  |")
+  print("| 2 |  Pessoa Jurídica                |")
+  print("-------------------:-------------------")
+
 
 
 def main():
@@ -30,7 +37,7 @@ def main():
     menu()
     opc = input("Informe uma opcao: \n")
 
-    if opc == "1":      
+    if opc == "1":      #Cadastrar Usuario   
       pass
 
     elif opc == "2":    
@@ -61,6 +68,43 @@ def main():
     input("Pressione ENTER para continuar!\n")
 
 
+
+def cadastroUser():
+  while opc != 1 and 2:
+    os.system('clear') or None
+    menu1()
+    opc = input("Informe uma opcao: \n")
+
+    if opc == 1:        #Fisica
+      nome =  input("Informe nome: ")
+      idade = input("Informe idade: ")
+      cpf = input("Informe cpf: ")
+      instEnsino = input("Informe Instuição de ensino: ")
+      
+      data = {"nome": nome, "idade": idade, "cpf": cpf, "instEnsino": instEnsino}
+      requests.post(f"{url}/addfisica", json=data)
+
+    elif opc == 2:      #Juridica
+      nome =  input("Informe nome: ")
+      segmento = input("Informe idade: ")
+      cnpj = input("Informe cpf: ")      
+
+      data = {"nome": nome, "segmento": segmento, "cnpj": cnpj}
+      requests.post(f"{url}/addjuridica", json=data)
+
+    else:
+      print("Opção invalida!")
+      input("Pressione ENTER para continuar!\n")
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
 # cliente = {"nome": "Joao Pedro", "endereco": "Rua XYZ"}
 # requests.post(f"{url}/cliente", json=cliente)
 
@@ -69,6 +113,3 @@ def main():
 
 # r = requests.get(f"{url}/cliente")
 # pprint(r.json())
-
-if __name__ == "__main__":
-    main()
